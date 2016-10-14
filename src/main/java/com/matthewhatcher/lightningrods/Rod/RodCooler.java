ec406d4e775c35e040eb888f74147fe6fc1422e1
Matthew Hatcher <hello@matthewh.in>
2016-10-14 00:39:25 -0400

    Adding documentation.

--- src/main/java/com/matthewhatcher/lightningrods/Rod/RodCooler.java
+++ src/main/java/com/matthewhatcher/lightningrods/Rod/RodCooler.java
@@ -14,19 +14,37 @@ public class RodCooler
 		this.cooldownTime = time;
 	}
 	
+	/**
+	 * Start cooling a player.
+	 * @param p
+	 */
 	public void startCooling(Player p) {
 		this.players.put(p.getName(), System.currentTimeMillis());
 	}
 	
+	/**
+	 * Get the remaining cooldown time for a player.
+	 * @param p
+	 * @return
+	 */
 	public long getRemainingTime(Player p) {
 		return ((this.players.get(p.getName()) / 1000) + this.cooldownTime) - (System.currentTimeMillis() / 1000);
 	}
 	
+	/**
+	 * Remove a player from cooldown.
+	 * @param p
+	 */
 	public void removePlayer(Player p) {
 		if(this.players.containsKey(p.getName()))
 			this.players.remove(p.getName());
 	}
 	
+	/**
+	 * Check if a player is cooling or not.
+	 * @param p
+	 * @return
+	 */
 	public boolean isCooling(Player p) {
 		if(!this.players.containsKey(p.getName()))
 			return false;
